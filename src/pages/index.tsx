2e35e51186b6369ecba496f6ae582867065cd3cd
naoya <sn7sn8@gmail.com>
2021-05-29 21:01:00 +0900

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -28,7 +28,7 @@ export default function Home() {
         <div className="border-b w-full pb-2 mb-8">
           <h1 className="inline-block font-bold text-4xl text-blue-500 tracking-widest">顧客一覧表</h1>
           <Link href="/daily"><a className="bg-indigo-700 text-white p-2 ml-8 duration-300 rounded-md hover:bg-indigo-600 text-md">当日来店顧客</a></Link>
-          {<button className="bg-pink-600 text-white p-2 ml-4 duration-300 rounded-md hover:bg-pink-500 text-md" onClick={openCamera}>QRコード読取</button>}
+          <button className="bg-pink-600 text-white p-2 ml-4 duration-300 rounded-md hover:bg-pink-500 text-md" onClick={openCamera}>QRコード読取</button>
         </div>
         <CustomerTable />
       </div>
