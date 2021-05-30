664340f5084f012bad7677a97f9bf198bf7d5401
naoya <sn7sn8@gmail.com>
2021-05-30 11:03:57 +0900

    fix

--- src/components/QrRead.tsx
+++ src/components/QrRead.tsx
@@ -26,10 +26,10 @@ const QrRead = ({isOpened, setIsOpened}) => {
 
 
   return (
-    <div className="fixed z-100 top-0 left-0 w-2/3">
+    <div className="fixed z-100 top-0 left-0 w-2/3" style={{width: "40%", left: "30%"}}>
       <QrReader delay={300} onScan={onScan} onError={onError} className="w-full" />
       <div className="px-10">
-        <button className="bg-pink-600 text-white p-2 mt-10 duration-300 rounded-md hover:bg-pink-500 text-md w-full" onClick={closeCamera}>閉じる</button>
+        <button className="bg-indigo-600 text-white p-2 mt-10 duration-300 rounded-md hover:bg-indigo-500 text-md w-full" onClick={closeCamera}>閉じる</button>
       </div>
     </div>
   )
