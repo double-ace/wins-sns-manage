ed3ea2913d2c35d6f7fee9afbee307d1ad727363
naoya9411 <sn7sn8@gmail.com>
2022-05-11 22:54:25 +0900

    リファクタ

--- src/pages/post/create.tsx
+++ src/pages/post/create.tsx
@@ -1,76 +1,83 @@
 import { useState, useEffect } from "react";
+import { Button } from "@mantine/core";
 import { Layout } from "../../components/Layout";
-import Link from "next/link";
 import toast, { Toaster } from "react-hot-toast";
 import { requestHttpPost } from "src/utils/requestBase";
 
-export default function Home() {
-  const [title, setTitle] = useState("");
-  const [content, setContent] = useState("");
+const POST_MESSAGE = {
+  confirm: "投稿しますか？",
+  success: "投稿しました",
+  error: "投稿に失敗しました",
+};
 
-  useEffect(() => {
-    if (!localStorage.getItem("access")) {
-      window.location.href = "/signin";
-    }
-  });
+const initPost = {
+  title: "",
+  content: "",
+};
+
+export default function Home() {
+  const [post, setPost] = useState({ ...initPost });
+  const [isPosting, setIsPosting] = useState(false);
 
   const handlePost = async () => {
+    setIsPosting(true);
     const param = {
-      title,
-      content,
+      title: post.title,
+      content: post.content,
     };
     console.log(param);
     console.log(localStorage.getItem("access"));
-    try {
-      const res = await requestHttpPost("/owner/posts/", param);
-      console.log(res.data);
-      toast.success("投稿しました");
-      setTitle("");
-      setContent("");
-    } catch {
-      toast.error("投稿に失敗しました");
+    const res = await requestHttpPost("/owner/posts/", param);
+    if (res.result) {
+      setPost(initPost);
+      toast.success(POST_MESSAGE.success);
+    } else {
+      toast.error(POST_MESSAGE.error);
     }
+    setIsPosting(false);
   };
 
   return (
     <Layout>
-      <div className="p-8 mx-auto">
+      <div className="pt-2">
         <Toaster />
-        <div className="border-b w-full pb-2 mb-8">
-          <h1 className="inline-block font-bold text-4xl text-blue-500 tracking-widest mr-8">
+        <div className="border-b w-full pb-2 mb-4">
+          <h1 className="inline-block text-2xl font-bold text-cyan-600 tracking-widest">
             投稿作成
           </h1>
-          <Link href="/">
-            <a className="bg-indigo-700 text-white p-2 duration-300 rounded-md hover:bg-indigo-600 w-28 text-md">
-              顧客一覧表
-            </a>
-          </Link>
         </div>
-        <div className="mx-auto px-4 max-w-6xl">
+        <div className="mx-auto mt-8 px-4 max-w-6xl">
           <div className="mb-6">
             <h2 className="mb-2">タイトル</h2>
             <input
               type="text"
               className="w-3/5 p-2 border rounded-md"
-              value={title}
-              onChange={(e) => setTitle(e.target.value)}
+              value={post.title}
+              onChange={(e) =>
+                setPost((prev) => ({ ...prev, title: e.target.value }))
+              }
             />
           </div>
           <div className="mb-4">
             <h2 className="mb-2">内容</h2>
             <textarea
               className="w-full p-2 border rounded-md"
-              value={content}
+              value={post.content}
               rows={10}
-              onChange={(e) => setContent(e.target.value)}
+              onChange={(e) =>
+                setPost((prev) => ({ ...prev, content: e.target.value }))
+              }
             ></textarea>
           </div>
-          <button
-            className="py-2 px-8 rounded-3xl text-white bg-indigo-900 hover:bg-indigo-700"
+          <Button
+            size="sm"
+            radius="xl"
+            loading={isPosting}
+            className="px-8 bg-cyan-600 hover:bg-cyan-500"
             onClick={handlePost}
           >
             投稿
-          </button>
+          </Button>
         </div>
       </div>
     </Layout>
