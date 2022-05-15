33b3c432a15c0247ea533a9758299bdca9457f8d
naoya9411 <sn7sn8@gmail.com>
2022-05-16 00:51:31 +0900

    チャット一覧＆チャット画面作成

---
+++ src/pages/chat/[roomId].tsx
@@ -0,0 +1,118 @@
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
+import { Avatar, Button } from "@mantine/core";
+import { Layout } from "src/components/Layout";
+import dayjs from "dayjs";
+import { requestHttpGet, requestHttpPost } from "src/utils/requestBase";
+
+type Message = {
+  id: string;
+  content: string;
+  sender: {
+    id: string;
+    profile_id: string;
+    family_name: string;
+    first_name: string;
+    nickname: string;
+    profile_image: string;
+  };
+  me: string;
+  room: string;
+  created_at: string;
+};
+
+const ChatRoom = () => {
+  const router = useRouter();
+  const [msg, setMsg] = useState<Message[]>([]);
+  const [newMsg, setNewMsg] = useState("");
+  const [isSending, setIsSending] = useState(false);
+  const [username, setUsername] = useState("");
+
+  useEffect(() => {
+    getChat(true);
+  }, []);
+
+  const getChat = async (reqUser: boolean) => {
+    const { roomId, profileId } = router.query;
+    const msgRes = await requestHttpGet(`/chat/messages/?room_id=${roomId}`);
+    msgRes.result && setMsg([...msgRes.data]);
+    if (reqUser) {
+      const nameRes = await requestHttpGet(`/user/name/${profileId}/`);
+      if (nameRes.result) {
+        const { family_name, first_name, nickname } = nameRes.data;
+        setUsername(`${family_name} ${first_name} (${nickname})`);
+      }
+    }
+  };
+
+  const sendMsg = async () => {
+    setIsSending(true);
+    const param = {
+      content: newMsg,
+      room: router.query.roomId.toString(),
+    };
+    const res = await requestHttpPost("/chat/messages/", param);
+    if (res.result) {
+      getChat(false);
+      setNewMsg("");
+    }
+    setIsSending(false);
+  };
+
+  const messages = msg.map((item: Message) => {
+    const { id, content, sender, me, created_at } = item;
+    return sender.id === me ? (
+      <div className="mb-4 flex flex-col items-end" key={id}>
+        <div className="mr-2 flex flex-col items-end">
+          <p className="bg-cyan-100 rounded-xl max-w-3xl p-2">{content}</p>
+          <div>
+            <p className="text-sm text-slate-400">
+              {dayjs(created_at).format("YYYY/MM/DD HH:mm:ss")}
+            </p>
+          </div>
+        </div>
+      </div>
+    ) : (
+      <div className="flex mb-4" key={id}>
+        <Avatar radius="xl" color="cyan" src={sender.profile_image} />
+        <div className="ml-2 flex flex-col items-start">
+          <p className="bg-slate-100 rounded-xl max-w-3xl p-2">{content}</p>
+          <p className="text-sm text-slate-400">
+            {dayjs(created_at).format("YYYY/MM/DD HH:mm:ss")}
+          </p>
+        </div>
+      </div>
+    );
+  });
+
+  return (
+    <Layout>
+      <div className="pt-2 mb-32">
+        <div className="border-b w-full pb-2 mb-4">
+          <h1 className="inline-block text-2xl font-bold text-cyan-600 tracking-widest">
+            {username}
+          </h1>
+        </div>
+        <div className="mx-auto mt-8">{messages}</div>
+      </div>
+      <div className="fixed p-2 md:pl-40 bottom-0 left-0 right-0 border-t bg-white">
+        <textarea
+          className="w-full border p-2"
+          value={newMsg}
+          onChange={(e) => setNewMsg(e.target.value)}
+        />
+        <Button
+          radius="xl"
+          size="md"
+          loading={isSending}
+          className="px-6 bg-cyan-600 hover:bg-cyan-500 float-right"
+          onClick={sendMsg}
+        >
+          送信
+        </Button>
+      </div>
+    </Layout>
+  );
+};
+
+export default ChatRoom;
---
+++ src/pages/chat/index.tsx
@@ -0,0 +1,77 @@
+import { useState, useEffect } from "react";
+import Link from "next/link";
+import { Avatar } from "@mantine/core";
+import { Layout } from "src/components/Layout";
+import dayjs from "dayjs";
+import { requestHttpGet } from "src/utils/requestBase";
+
+type ChatRoom = {
+  id: string;
+  status: string;
+  creator: {
+    id: string;
+    profile_id: string;
+    family_name: string;
+    first_name: string;
+    nickname: string;
+    profile_image: string;
+  };
+  member: string;
+  updated_by: string;
+  updated_at: string;
+  message: {
+    id: string;
+    content: string;
+    sender: string;
+    room: string;
+    created_at: string;
+  };
+};
+
+const ChatRoomList = () => {
+  const [roomList, setRoomList] = useState<ChatRoom[]>([]);
+
+  useEffect(() => {
+    getChat();
+  }, []);
+
+  const getChat = async () => {
+    const res = await requestHttpGet("/chat/rooms/");
+    res.result && setRoomList([...res.data]);
+  };
+
+  const chats = roomList.map((item: ChatRoom) => {
+    const { id, status, creator, updated_at, message } = item;
+    return (
+      <Link href={`/chat/${id}?profileId=${creator.profile_id}`} key={id}>
+        <a className="block p-2 border-b hover:bg-slate-100">
+          <div className="flex">
+            <Avatar radius="xl" color="cyan" src={creator.profile_image} />
+            <div className="ml-4">
+              <p className="text-lg text-slate-700">{`${creator.family_name} ${creator.first_name} (${creator.nickname})`}</p>
+              <p className="text-md text-slate-600">{message.content}</p>
+              <p className="text-xs text-slate-500">
+                {dayjs(updated_at).format("YYYY/MM/DD HH:mm:ss")}
+              </p>
+            </div>
+          </div>
+        </a>
+      </Link>
+    );
+  });
+
+  return (
+    <Layout>
+      <div className="pt-2">
+        <div className="border-b w-full pb-2 mb-4">
+          <h1 className="inline-block text-2xl font-bold text-cyan-600 tracking-widest">
+            投稿一覧
+          </h1>
+        </div>
+        <div className="mx-auto md:mx-16 mt-8 px-4 max-w-4xl">{chats}</div>
+      </div>
+    </Layout>
+  );
+};
+
+export default ChatRoomList;
